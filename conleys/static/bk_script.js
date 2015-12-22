ccb603dcfde233e055ca89f73268e4362021bfa7
Qteb <qtebest@gmail.com>
2015-12-22 21:56:44 +0300

    reinit conleys

--- conleys/static/bk_script.js
+++ conleys/static/bk_script.js
@@ -1,23 +1,23 @@
 /*
-** Blätterkatalog Integration Script
+** Blï¿½tterkatalog Integration Script
 ** *********************************
 **
 ** 1) 	addToCart(artno, shopsessionid)
 **	Wird bei Linktyp "Artikel in Warenkorb legen"
 **	aufgerufen.
 **
-** 
+**
 ** 2) 	blaetterkatalogflip(newPage)
-**	Blättert auf die gegebene Seite
+**	Blï¿½ttert auf die gegebene Seite
 **	Wird bei Linktyp "Gehe zu Seite" verwendet.
 **
 **	TODO: Kann optional verwendet werden
-**	      um mittels 
-**	
+**	      um mittels
+**
 **	<a href="javascript:blaetterkatalogflip(5)">Gehe zu Seite 5</a>
 **
-**	den Blätterkatalog umzublättern.
-**	(Beispiel: Auf Kapitel-Startseite blättern)
+**	den Blï¿½tterkatalog umzublï¿½ttern.
+**	(Beispiel: Auf Kapitel-Startseite blï¿½ttern)
 **
 ** 3)   gotoURL(myURL)
 **   	Wird bei Linktyp "Externer Link" und
@@ -27,23 +27,23 @@
 	/**
 	*/
 	function trackPageHit(page) {
-	} 
-	
+	}
+
 	function addToCart(artno, shopsessionid)
 	{
 	   var destinationURL = shopBase + shopURL + shopSessionIDParameterName + shopsessionid + shopArticleIDParameterName + artno + shopURLPostfix;
-	   
-	   if (shopDebug) alert("ShopSessionID: "+shopsessionid+"\nArtikel "+artno+" in Detailansicht Shop aufrufen\n\nSHOP URL: "+destinationURL);	
-		
-	   
+
+	   if (shopDebug) alert("ShopSessionID: "+shopsessionid+"\nArtikel "+artno+" in Detailansicht Shop aufrufen\n\nSHOP URL: "+destinationURL);
+
+
 	   if (!shopDebug) gotoURL(destinationURL);
-		
+
 	}
 
 
 	/**
-	*  blättert auf die entsprechende seite um
-	*  setzt voraus, dass der blätterkatalog in
+	*  blï¿½ttert auf die entsprechende seite um
+	*  setzt voraus, dass der blï¿½tterkatalog in
 	*  dem fenster noch geladen ist
 	*/
 	function blaetterkatalogflip(newPage)
@@ -51,12 +51,12 @@
 		flashCommand('jump_to_id', newPage)
 	}
 
-	
+
 	/**
 	* sprint zu einer URL
 	* Ziel: Shop-Fenster
-	*/ 
-	function gotoURL( myURL ) 
+	*/
+	function gotoURL( myURL )
 	{
 		if (myURL.indexOf("mailto:")!=-1) {
 			location.href=myURL;
@@ -69,24 +69,24 @@
 			if ( typeof( opener ) == "undefined" || typeof( opener ) == "unknown" ||
 			    	typeof( opener.name ) == "undefined" || typeof( opener.name ) == "unknown" )
 			{
-				if ( shopFenster.closed )	
+				if ( shopFenster.closed )
 				{
 					shopFenster = window.open( myURL, "shop", "" );
 					shopFenster.focus();
 				}
-				else 
+				else
 				{
 					shopFenster.location.href=myURL;
-					shopFenster.focus();	
+					shopFenster.focus();
 				}
-			} 
-			else 
+			}
+			else
 			{
 				opener.location.href = myURL;
 				opener.blatterkatalogPopup = window;
 				opener.focus();
 			}
-		} 
+		}
 		catch ( Exception )
 		{
 			// if( opener && !opener.closed )
@@ -94,20 +94,20 @@
 			// 	opener.location.href = myURL;
 			// 	opener.focus();
 			// }
-			// else 
+			// else
 			// {
 				try {
-					if ( shopFenster.closed )	
+					if ( shopFenster.closed )
 					{
 						shopFenster = window.open( myURL, "shop", "" );
 						shopFenster.focus();
 					}
-					else 
+					else
 					{
 						shopFenster.location.href = myURL;
-						shopFenster.focus();	
+						shopFenster.focus();
 					}
-				} 
+				}
 				catch ( Exception )
 				{
 						shopFenster = window.open( myURL, "shop", "" );
@@ -120,22 +120,22 @@
 	/**
 	 * getFlashPath() ermittelt das Flash-Objekt
 	 */
-	function getFlashPath() 
+	function getFlashPath()
 	{
-    	if ( document.all ) 
+    	if ( document.all )
     	{
 	        //ie
 	        return document.bkflash;
     	}
-    	else 
+    	else
     	{
 	        //safari
 	        var ffilm = document.getElementById( "bkflash" );
-	        try 
+	        try
 	        {
 	            flashfilm.SetVariable( "tst", "1" );
-	       	} 
-	       	catch ( Exception ) 
+	       	}
+	       	catch ( Exception )
 	       	{
 	            //firefox
             	ffilm = document.embeds["bkflash"];
@@ -143,8 +143,8 @@
             return ffilm;
 	    }
 	}
-	
-	
+
+
 
 	/////////////////////////////////////
 	// Funktion zum externen Aufruf der Flash-Commands
@@ -152,73 +152,73 @@
 	// par => pageid
 	// aufruf mit z.b. flashCommand('jump_to_id', 5)
 	/////////////////////////////////////
-	function flashCommand( id, par ) 
+	function flashCommand( id, par )
 	{
 		try
 		{
 		    var movie = getFlashPath();
 		    movie.SetVariable( "jscommand", id + "|" + par );
-		}   
+		}
 	    catch( Exception )
 	    {
-	      	document.getElementById("bkflash").SetVariable( "jscommand", id + "|" + par );		
-	    }	
+	      	document.getElementById("bkflash").SetVariable( "jscommand", id + "|" + par );
+	    }
 	}
-	
-	
-	function getURLParam( strParamName, defaultBKValue ) 
+
+
+	function getURLParam( strParamName, defaultBKValue )
 	{
-	var strReturn = " ";
-	var strHref = window.location.href;
-	
-	var i = strHref.indexOf( strParamName );
-	
-	if ( i > 0 )
-	{
-		var j = strHref.indexOf( "=", i + 1 );
-		if ( j > 0 )
+		var strReturn = " ";
+		var strHref = window.location.href;
+
+		var i = strHref.indexOf( strParamName );
+
+		if ( i > 0 )
 		{
-			var endPos = strHref.length;
-			var k = strHref.indexOf( "?", j + 1 );
-			var l = strHref.indexOf( "&", j + 1 );
-			if ( k > 0 && l > 0 )
+			var j = strHref.indexOf( "=", i + 1 );
+			if ( j > 0 )
 			{
-			   	if ( k < l ) 
-			   	{
-			   		endPos = k;
-			   	}	
-				else 
+				var endPos = strHref.length;
+				var k = strHref.indexOf( "?", j + 1 );
+				var l = strHref.indexOf( "&", j + 1 );
+				if ( k > 0 && l > 0 )
 				{
-					endPos = l;
-				}	
-			} 
-			else if ( k > 0 )
-			{
-			  endPos = k;
-			} 
-			else if ( l > 0 )
-			{
-			  endPos = l;
+					if ( k < l )
+					{
+						endPos = k;
+					}
+					else
+					{
+						endPos = l;
+					}
+				}
+				else if ( k > 0 )
+				{
+				  endPos = k;
+				}
+				else if ( l > 0 )
+				{
+				  endPos = l;
+				}
+
+				strReturn = strHref.substring( j + 1 , endPos );
 			}
-			
-			strReturn = strHref.substring( j + 1 , endPos );
-		}	
+		}
+
+		if (strReturn == "" || strReturn == " ")
+		{
+			if (defaultBKValue)
+			{
+				return defaultBKValue;
+			}
+		}
+
+		return strReturn;
 	}
-	
-	if (strReturn == "" || strReturn == " ") 
-  {
-     if (defaultBKValue)
-     {
-       return defaultBKValue;
-     }
-  }
-	
-	return strReturn;
-	}	
 
 	window.onerror = stoperror;
 
 	function stoperror( a )
 	{
-  	return false;
+  		return false;
 	}
