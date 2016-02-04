07cc38d8da775ab2c96ab694c0d151f7d5a28bd9
Qteb <qtebest@gmail.com>
2016-02-04 18:09:02 +0300

    added restart script

---
+++ restartAll.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+array=(
+madeleine
+klingel
+peterhahn
+impressionen
+conleys
+gingar
+faibels
+miavilla
+schneider
+zalando
+)
+
+for i in "${array[@]}"
+do
+        echo -ne "Now restarting: $i"\\n
+        /usr/sbin/service $i stop > /dev/null
+        sleep 0.5
+        /usr/sbin/service $i start > /dev/null
+        #echo -ne "             - done!"\\n
+done
