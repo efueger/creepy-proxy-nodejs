0ae93201a14a89840e6b7cebb07e693358a00def
Qteb <qtebest.work@gmail.com>
2016-04-01 14:48:44 +0300

    moving to new head

--- peterhahn/data/includes.js
+++ peterhahn/data/includes.js
@@ -5,18 +5,18 @@ module.exports = function(catalog, param, options){
             '<meta name="google-translate-customization" content="43fe27a3bb3e2250-8cec14ecfd68c655-gf1c6d92e0168ae14-12" />',
             '<meta name="robots" content="noindex, nofollow"/>',
             '<link href="http://www.'+catalog+'.catalogi.ru/static/override.css" rel="stylesheet" type="text/css" />',
-            '<link href="http://cdn.catalogi.ru/static/css/jquery.jscrollpane.css" rel="stylesheet" type="text/css" />',
-            '<link href="http://cdn.catalogi.ru/static/css/jquery.colorbox.css" rel="stylesheet" type="text/css" />',
-            '<link href="http://cdn.catalogi.ru/static/css/validationEngine.jquery.css" rel="stylesheet" type="text/css" />',
+            '<link href="http://test3.catalogi.ru/static/css/jquery.jscrollpane.css" rel="stylesheet" type="text/css" />',
+            '<link href="http://test3.catalogi.ru/static/css/jquery.colorbox.css" rel="stylesheet" type="text/css" />',
+            '<link href="http://test3.catalogi.ru/static/css/validationEngine.jquery.css" rel="stylesheet" type="text/css" />',
             '<!--<link href="http://www.'+catalog+'.catalogi.ru/static/common.css" rel="stylesheet" type="text/css" />-->',
             '<script type="text/javascript" src="http://yandex.st/jquery/2.1.1/jquery.min.js"></script>',
-            '<script type="text/javascript" src="http://cdn.catalogi.ru/static/js/jquery.mousewheel.js"></script>',
-            '<script type="text/javascript" src="http://cdn.catalogi.ru/static/js/jquery.jscrollpane.min.js"></script>',
-            '<script type="text/javascript" src="http://cdn.catalogi.ru/static/js/jquery.colorbox-min.js"></script>',
-            '<script type="text/javascript" src="http://cdn.catalogi.ru/static/js/jquery.validationEngine-ru.js"></script>',
-            '<script type="text/javascript" src="http://cdn.catalogi.ru/static/js/jquery.validationEngine.js"></script>',
-            '<script type="text/javascript" src="http://cdn.catalogi.ru/static/js/jquery.cookie.js"></script>',
-            '<script type="text/javascript" src="http://cdn.catalogi.ru/static/js/common.js"></script>',
+            '<script type="text/javascript" src="http://test3.catalogi.ru/static/js/jquery.mousewheel.js"></script>',
+            '<script type="text/javascript" src="http://test3.catalogi.ru/static/js/jquery.jscrollpane.min.js"></script>',
+            '<script type="text/javascript" src="http://test3.catalogi.ru/static/js/jquery.colorbox-min.js"></script>',
+            '<script type="text/javascript" src="http://test3.catalogi.ru/static/js/jquery.validationEngine-ru.js"></script>',
+            '<script type="text/javascript" src="http://test3.catalogi.ru/static/js/jquery.validationEngine.js"></script>',
+            '<script type="text/javascript" src="http://test3.catalogi.ru/static/js/jquery.cookie.js"></script>',
+            '<script type="text/javascript" src="http://test3.catalogi.ru/static/js/common.js"></script>',
             '<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=_googleTranslateElementInit"></script>',
             '<script type="text/javascript" src="http://www.'+catalog+'.catalogi.ru/static/common.js"></script>'
         ].join("\n"),
@@ -28,7 +28,7 @@ module.exports = function(catalog, param, options){
         body: {
             top: [
                 '<div id="google_translate_element" style="display: none"></div>',
-                '<iframe src="http://cdn.catalogi.ru/executable/index.php?'+param+'" width="'+options.width+'px" height="'+options.height+'px" frameborder="0" name="_head" id="iframe"/>'
+                '<iframe src="http://test3.catalogi.ru/executable/index.php?'+param+'" width="'+options.width+'px" height="'+options.height+'px" frameborder="0" name="_head" id="iframe"/>'
             ].join("\n"),
 
             bottom: [
