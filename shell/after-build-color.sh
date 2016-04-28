35e95948292c7c547ebc5112c54af71018126bab
Qteb <qtebest.work@gmail.com>
2016-04-28 17:29:27 +0300

    added config

--- shell/after-build-color.sh
+++ shell/after-build-color.sh
@@ -3,15 +3,20 @@
 domainspath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 source $domainspath/shell/settings.cfg
 
+NC='\033[0m' # No Color
+Red='\033[0;31m'
+Green='\033[0;32m'
+LightCyan='\033[1;36m'
+
 echo "checking for required folders..."
 for domain in "${domains[@]}"
 do
-	echo -ne $domain\\n
+	echo -ne ${LightCyan}$domain${NC}\\n
 	cd $domainspath/$domain/data
 
 	echo -ne "	creating symbolic link..."
 	ln -s $domainspath/$domain/data/init /etc/init.d/$domain > /dev/null 2>&1
-	echo -ne "		done"\\n
+	echo -ne ${Green}"		done"${NC}\\n
 
 	for item in "${dirs[@]}"
 	do
@@ -19,9 +24,9 @@ do
 		mkdir -p $item
 		echo -ne ", owner: $owner..."
 		chown $owner:$owner $item
-		echo -ne "	done"\\n
+		echo -ne ${Green}"	done"${NC}\\n
 	done
 done
 
-echo -ne \\n"total domains checked: "${#domains[@]}
-echo -ne \\n"done"\\n\\n
+echo -ne \\n"total domains checked: "${LightCyan}${#domains[@]}${NC}
+echo -ne \\n"${Green}done${NC}"\\n\\n
--- shell/restart-domains-color.sh
+++ shell/restart-domains-color.sh
@@ -3,15 +3,20 @@
 domainspath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 source $domainspath/shell/settings.cfg
 
+NC='\033[0m' # No Color
+Red='\033[0;31m'
+Green='\033[0;32m'
+LightCyan='\033[1;36m'
+
 echo "relaunching domains..."
 
 for domain in "${domains[@]}"
 	do
-		echo -ne "	now restarting: "$domain\\n
+		echo -ne "	now restarting: "${LightCyan}$domain${NC}\\n
 		/usr/sbin/service $domain stop > /dev/null 2>&1
 		sleep 5
 		/usr/sbin/service $domain start > /dev/null 2>&1
 	done
 
-echo -ne \\n"total domains restarted: "${#domains[@]}
-echo -ne \\n"done"\\n
+echo -ne \\n"total domains restarted: "${LightCyan}${#domains[@]}${NC}
+echo -ne \\n"${Green}done${NC}"\\n
