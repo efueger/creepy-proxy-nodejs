b316f7cc685f2d0e99e2bd28bd12480e7ece5ebe
Qteb <qtebest.work@gmail.com>
2016-04-28 18:58:49 +0300

    small fixes

--- issuu/static/common.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by mihailstepancenko on 28.04.16.
- */
-
-window.navigator.__defineGetter__('userAgent', function () {
-    return 'Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B137 Safari/601.1';
-});
