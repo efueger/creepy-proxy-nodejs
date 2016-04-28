4fd8927b95085ce1e29c0ebe73d931288cb0ed2b
Qteb <qtebest.work@gmail.com>
2016-04-28 12:28:35 +0300

    init ateliergs

---
+++ ateliergs/static/customlib.js
@@ -0,0 +1,66 @@
+//OPEN LINK
+function onlinekat_open(linkContent, linkTarget, payload) {
+
+		switch(linkTarget){
+			case  "_javascript":
+			{
+				linkContent = "http://www.janvanderstorm.catalogi.ru/janvanderstorm/de/s?_sb=true&query="+linkContent;
+				window.open(linkContent, "_top");
+				break;
+			}
+      case  "text":
+			{
+				linkContent = "http://www.janvanderstorm.catalogi.ru/janvanderstorm/de/s?_sb=true&query="+linkContent;
+				window.open(linkContent, "_top");
+				break;
+			}
+			case "_deeplink":
+			{
+				window.open(linkContent, "_blank");
+				break;
+			}
+			case "_webpage":
+			{
+				window.open(linkContent, "_blank");
+				break;
+			}
+			case "_self":
+			{
+				window.open(linkContent, "_self");
+				break;
+			}
+			case "_blank":
+			{
+				window.open(linkContent, "_blank");
+				break;
+			}
+			case "_popup":
+			{
+				window.open(linkContent, '_blank','width=800,height=600,menubar=no,status=no,locationbar=no,scrollbars=no,resizable=yes');
+				break;
+			}
+			default:
+			{
+				if (window.opener) {
+					try {
+						window.opener.location.href = linkContent;
+						window.opener.focus();
+					}
+					catch (e) {
+						window.open(linkContent, linkTarget);
+					}
+				}
+				else {
+					window.open(linkContent, linkTarget);
+				}
+			}//end default
+		}//and switch
+}
+function addToBasket(linkContent) {
+		console.log(linkContent);
+		top.location.href = 'http://www.janvanderstorm.catalogi.ru/shop/s?query='+linkContent+'&searchSubmit=&fulltext-search=true&fromSearch=true';
+}
+function sendInformationToAnalytics(category, action, opt_label)
+{
+	_gaq.push(['_trackEvent', category, action, opt_label]);
+}
